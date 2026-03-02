b196922bce78e6cdd85c55c9475f553b14eea131
Kamil Sobania <kamil.sobania.97@gmail.com>
2026-03-02 08:12:02 +0100

    włącza sie na 2025

---
+++ HFT_DrawingHelper/App.xaml.cs
@@ -0,0 +1,7 @@
+﻿namespace HFT_DrawingHelper {
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App {
+    }
+}
---
+++ HFT_DrawingHelper/MainWindow.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using System.IO;
+using System.Collections;
+
+using TSM = Tekla.Structures.Model;
+using TSG = Tekla.Structures.Geometry3d;
+using TS = Tekla.Structures;
+using TSD = Tekla.Structures.Drawing;
+using TSMO = Tekla.Structures.Model.Operations;
+
+namespace HFT_DrawingHelper {
+    public partial class MainWindow {
+        public MainWindow() {
+            var myModel = new TSM.Model();
+            
+            if (myModel.GetConnectionStatus()) {
+                InitializeComponent();
+                ModelDrawingLabel.Content = myModel.GetInfo().ModelName.Replace(".db1", "");
+            }
+            else MessageBox.Show("Keine Verbindung zu Tekla Structures");
+        }
+    }
+}
